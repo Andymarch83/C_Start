4df8375c5a3d302bdac36a680a471e55b687c0b7
Andymarch83 <122732408+Andymarch83@users.noreply.github.com>
2024-02-01 18:37:42 +0300

    Add files via upload

---
+++ Sem_1/sciiart_sem_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf(".--.  /\\             ____ \n");
+	printf("'--' /__\\ (^._.^)~ <(o.o )> \n\n");
+	printf("  |\\_/| \n");
+	printf(" / @ @ \\ \n");
+	printf("( > \x48 < ) \n");
+	printf(" \x27>>x<<\x27 \n");
+	printf("  / O \\  \x21\n");
+	return 0;
+}
+
