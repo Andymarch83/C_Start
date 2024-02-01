4b4cf9c6aa6101bc8e09dff04d988fae01827cc0
Andymarch83 <122732408+Andymarch83@users.noreply.github.com>
2024-02-01 18:27:41 +0300

    Add files via upload

---
+++ Home_1/house.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	// printf("   *\n  ***\n *****\n*******\n HH HH\n ZZZZZ\n");
+	printf("   *\n");
+	printf("  ***\n");
+	printf(" *****\n");
+	printf(" HH HH\n ZZZZZ\n");
+	printf(" ZZZZZ\n");
+	return 0;
+}
+
---
+++ Home_1/lgtw.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	// printf("Let\x27s\n  go\n   to walk\n");
+	printf("Let\x27s\n");
+	printf("  go\n");
+	printf("   to walk\n");
+	return 0;
+}
+
