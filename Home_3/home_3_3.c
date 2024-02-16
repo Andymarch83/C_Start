339ca4a0958e91791e1368585c03c3b8845cefd6
Andymarch83 <122732408+Andymarch83@users.noreply.github.com>
2024-02-16 15:28:08 +0300

    Update home_3_3.c

--- Home_3/home_3_3.c
+++ Home_3/home_3_3.c
@@ -3,6 +3,6 @@ int main(int argc, char **argv)
 {
 	int a,b,c;
 	scanf("%d%d%d",&a,&b,&c);
-	printf("%s",a < b && b <= c ? "YES" : "NO");
+	printf("%s",a <= b && b <= c ? "YES" : "NO");
 	return 0;
 }
