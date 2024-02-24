8dad04eb40683d9ba7a030c593b4a74641d96f9f
Andymarch83 <122732408+Andymarch83@users.noreply.github.com>
2024-02-24 18:08:12 +0300

    Update home_4_5.c

--- Home_4/home_4_5.c
+++ Home_4/home_4_5.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 int main() {
-	int number, i, sum, product;
+	int number, i, sum, product, currentNum;
 	scanf("%d", &number);
 	for (i = 10; i <= number; i++) {
 		sum = 0;
 		product = 1;
-		while (i > 0) {
-			sum += i % 10;
-			product *= i % 10;
-			i /= 10; 
+        currentNum = i;
+		while (currentNum > 0) {
+			sum += currentNum % 10;
+			product *= currentNum % 10;
+currentNum /= 10; 
 		}
-		if (sum == product && i <= number) {
-			printf("%d\n", i);
+		if (sum == product && currentNum <= number) {
+			printf("%d\n", currentNum);
 		}
 	}
 	return 0;
