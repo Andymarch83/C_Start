a2ae0a75feb8fd548161801a741787a3e03da6b2
Andymarch83 <122732408+Andymarch83@users.noreply.github.com>
2024-03-14 10:33:28 +0300

    Update home_6_2.c

--- Home_6/home_6_2.c
+++ Home_6/home_6_2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void print_numbers_b(int a, int b) 
+/*void print_numbers_b(int a, int b) 
 {
     if (a < b) 
     {
@@ -33,12 +33,26 @@ void print_numbers_a(int a, int b)
 	{
 		printf("%d ", a);
 	}
+}*/
+
+void print_numbers(int a, int b)
+{
+	if (a == b) 
+	{
+	printf("%d", a);
+	}
+	else
+	{
+	(a < b) ? printf("%d ", a++) : printf("%d ", a--);
+	print_numbers(a, b);
+	}
 }
 
 int main() {
     int a, b;
     scanf("%d %d", &a, &b);
-    (a < b)? print_numbers_b(a, b): print_numbers_a(a, b);
+    //(a < b)? print_numbers_b(a, b): print_numbers_a(a, b);
+    print_numbers(a, b);
     return 0;
 }
 
