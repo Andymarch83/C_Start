19ab154b4fce6561182f2c5e16ef020dca0f1266
Andymarch83 <122732408+Andymarch83@users.noreply.github.com>
2024-03-14 09:18:37 +0300

    Update home_6_1.c

--- Home_6/home_6_1.c
+++ Home_6/home_6_1.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-void revRec(int num)
+void printNumbers(int num)
 {
 	if(num == 0)
 		return;
-	revRec(num/10);
+	printNumbers(num/10);
 	printf("%d ", num%10);
 }
 int main() {
 	int n;
 	scanf("%d", &n);
-	revRec(n);
+	printNumbers(n);
 	return 0;
 }
 
