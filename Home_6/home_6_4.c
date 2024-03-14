1521a8d934be9384799d5bc89d21a5a836405e58
Andymarch83 <122732408+Andymarch83@users.noreply.github.com>
2024-03-14 10:35:25 +0300

    Update home_6_4.c

--- Home_6/home_6_4.c
+++ Home_6/home_6_4.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
 
-void recursion(int n) 
+void recursion() 
 {
     int input;
     scanf("%d", &input);
     if (input == 0) 
-		return;
-	if (input % 2 == 1) 
+	return;
+    if (input % 2 != 0) 
 	{
         printf("%d ", input);
-        recursion(input - 1);
-    } 
-    else 
-    {
-		recursion(input - 1);
-    }
+	} 
+    recursion();
+    
 }
 
 
 int main() {
-    recursion(1); 
+    recursion(); 
     return 0;
 }
 
