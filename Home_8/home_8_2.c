412389bd007d2c8cb5944df30a0b75bc55ad8fd5
Andymarch83 <122732408+Andymarch83@users.noreply.github.com>
2024-03-26 16:08:50 +0300

    Update home_8_2.c

--- Home_8/home_8_2.c
+++ Home_8/home_8_2.c
@@ -37,10 +37,10 @@ void sort_even_odd(int n, int a[]) {
 	int size;
     int arr[100];
 
-    printf("Enter the size of the array: ");
+    printf("Input size: ");
     scanf("%d", &size);
 
-    printf("Enter the elements of the array: ");
+    printf("Input numbers: ");
     for (int i = 0; i < size; i++) {
         scanf("%d", &arr[i]);
     }
